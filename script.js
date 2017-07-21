46e6190e8e70b0869c409dd5aa04851d789781c1
Devon Bull <dbull@live.com>
2017-07-21 00:14:44 -0600

    clearing on create, removed dead code

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 let createButton = document.getElementById('create')
 createButton.addEventListener('click', () => {
     let newScript = document.getElementById('editor').innerText
-    console.log(newScript)
     let script = document.createElement( 'script' );
-    // script.type = 'text/javascript';
     script.append(newScript)
     let placeToAppend = document.getElementById('code')
     placeToAppend.append( script );
+    document.getElementById('editor').innerText = ''
 })
